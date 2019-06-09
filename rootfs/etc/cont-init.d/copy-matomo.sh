106d80d889d8466b7fdf8d15de7f2b069639c57f
Knut Ahlers <knut@ahlers.me>
2019-06-09 16:12:47 +0200

    Fix location and add GeoIP database
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- rootfs/etc/cont-init.d/copy-matomo.sh
+++ rootfs/etc/cont-init.d/copy-matomo.sh
@@ -13,4 +13,10 @@ echo "No Matomo installation found or not up-to-date" >&2
 echo "Installing bundled Matomo version" >&2
 
 tar -xvz -C /var/www --strip-components=1 -f /opt/matomo.tgz
+
+if [[ -f misc/GeoLite2-City.mmdb ]]; then
+	curl -sSfL "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz" |
+		tar -xz -C misc --strip-components=1 --wildcards '*/GeoLite2-City.mmdb'
+fi
+
 chown -R nobody:nogroup /var/www
